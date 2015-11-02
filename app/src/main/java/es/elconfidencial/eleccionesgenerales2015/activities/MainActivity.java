f9f2bec878f175abe24fce0631b4ee438c4b65dc
JesusMSM <jesusm.sanchez93@gmail.com>
2015-11-02 19:50:11 +0100

    Añadida barra separadora spinner

--- app/src/main/java/es/elconfidencial/eleccionesgenerales2015/activities/MainActivity.java
+++ app/src/main/java/es/elconfidencial/eleccionesgenerales2015/activities/MainActivity.java
@@ -90,27 +90,7 @@ public class MainActivity extends AppCompatActivity {
         //GlobalMethod.dislikesCount = GlobalMethod.getMyHashmap(getApplicationContext(),"dislikesCount");
     }
 
-        @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onBackPressed() {
